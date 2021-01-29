897127e76372d0aa42763ca1e8992fbb9c15e822
Paul Realpe <paulmrg461@gmail.com>
2021-01-29 13:53:15 -0500

    78. Custom Middleware - Validate fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,15 +2,7 @@ const { response } = require('express');
 const { validationResult } = require('express-validator');
 
 const createNewUser = ( req, res = response ) => {
-    const errors = validationResult( req );
-
-    if( !errors.isEmpty() ){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
-
+    
     res.json({
         ok: true,
         msg: 'Create new user!'
--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { createNewUser } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
 router.post('/new', [
     check('name','The name is required.').not().isEmpty(),
+    check('email','The email is required.').not().isEmpty(),
+    check('email','Check your email format.').isEmail(),
+    check('password','The password is required.').not().isEmpty(),
+    check('password','Your password must be at least 6 characters.').isLength({ min: 6 }),
+    validateFields
 ], createNewUser);
 
 module.exports = router;
