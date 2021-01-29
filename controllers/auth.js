e82964948eba87888b28f6751a64bd01efe64f7e
Paul Realpe <paulmrg461@gmail.com>
2021-01-29 15:59:02 -0500

    80. Validate if email is already registered.

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,34 @@ const User = require('../models/user')
 
 const createNewUser = async ( req, res = response ) => {
 
-    const user = new User( req.body );
-    //Save new user in database
-    await user.save();
-    
-    res.json({
-        ok: true,
-        body: req.body
-    });
+    const { email } = req.body;
+
+    try {
+        const emailRegistered = await User.findOne({ email });
+
+        if(emailRegistered) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'This email is already registered'
+            });
+        }
+
+        const user = new User( req.body );
+        //Save new user in database
+        await user.save();
+        
+        res.json({
+            ok: true,
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'There was a problem! Please contact the administrator.'
+        })
+    }
+
 }
 
 module.exports = {
