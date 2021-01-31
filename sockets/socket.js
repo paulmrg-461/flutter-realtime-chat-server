555002edf7192cae706c6e61423de74e542247ae
Paul Realpe <paulmrg461@gmail.com>
2021-01-31 07:28:08 -0500

    107. Authenticating client connected by sockets

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,13 @@
+const { checkJWT } = require('../helpers/jwt');
 const { io } = require('../index');
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Client is online');
+    const [ valid, uid ] = checkJWT(client.handshake.headers['x-token']);
+    
+    if( !valid ) { return client.disconnect(); }
 
     client.on('disconnect', () => {
         console.log('Client is offline');
@@ -13,6 +17,4 @@ io.on('connection', client => {
         console.log('Message', payload);
         io.emit( 'mensaje', { admin: 'New message' } );
     }); */
-
-
 });
