1a026cba476a6fa642d9cdd5a61547f8a6626089
Paul Realpe <paulmrg461@gmail.com>
2021-01-31 07:43:05 -0500

    108. Update DB when user is online

---
+++ controllers/socket.js
@@ -0,0 +1,21 @@
+const User = require('../models/user');
+
+const userOnline = async ( uid = '' ) => {
+    const user = await User.findById( uid );
+    user.online = true;
+    await user.save();
+
+    return user;
+}
+const userOffline = async ( uid = '' ) => {
+    const user = await User.findById( uid );
+    user.online = false;
+    await user.save();
+
+    return user;
+}
+
+module.exports = {
+    userOnline, 
+    userOffline
+}
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,20 @@
 const { checkJWT } = require('../helpers/jwt');
 const { io } = require('../index');
-
+const { userOnline, userOffline } = require('../controllers/socket');
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Client is online');
     const [ valid, uid ] = checkJWT(client.handshake.headers['x-token']);
-    
+
+    //Check authentication
     if( !valid ) { return client.disconnect(); }
 
+    //Client authenticated
+    userOnline(uid);
+
     client.on('disconnect', () => {
-        console.log('Client is offline');
+        userOffline( uid );
     });
 
     /* client.on('mensaje', ( payload ) => {
